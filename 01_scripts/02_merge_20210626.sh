a052a31e525252b80f74d3d232a9707ed2ba8814
LaurieLecomte <lalec31@manitou.ibis.ulaval.ca>
2022-06-02 09:22:05 -0400

    removed irrelevant .gz extension and added compression step

--- 01_scripts/02_merge_20210626.sh
+++ 01_scripts/02_merge_20210626.sh
@@ -11,10 +11,13 @@ MERGED_DIR="04_merged"
 
 # 1. Create output file header based on one vcf file
 FIRST_CHR=$(less $CHR_LIST | head -n1)
-zgrep '^#' $OUT_DIR/"$FIRST_CHR".vcf.gz | grep -v '^##contig=<ID=scaf' > $MERGED_DIR/merged_20210626.vcf.gz
+zgrep '^#' $OUT_DIR/"$FIRST_CHR".vcf.gz | grep -v '^##contig=<ID=scaf' > $MERGED_DIR/merged_20210626.vcf
 
 # 2. Concat all VCFs together without their header lines beginning with '#' 
 cat $CHR_LIST | while read CHR
 do
-  zgrep -v '^#' $OUT_DIR/"$CHR".vcf.gz >> $MERGED_DIR/merged_20210626.vcf.gz
+  zgrep -v '^#' $OUT_DIR/"$CHR".vcf.gz >> $MERGED_DIR/merged_20210626.vcf
 done
+
+# 3. Compress 
+bgzip $MERGED_DIR/merged_20210626.vcf
